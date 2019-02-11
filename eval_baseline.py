d3508fdbbd6069570c21c4c5b84d111f91c3994e
HTAustin <socool.king@gmail.com>
2019-02-11 17:08:12 -0500

    Reorg code

--- eval_baseline.py
+++ eval_baseline.py
@@ -3,34 +3,34 @@ import numpy as np
 import operator
 
 def load_topK_doc_run(topK, topic, runF):
-	with open(runF) as rF:
-		topDoc = []
-		for line in rF:
-			t, _, sent, rank, score, run = line.split()
-			if topic == t and int(rank) <= topK:
-				doc = sent.split('_')[0]
-				topDoc.append(doc)
-	return topDoc
+    with open(runF) as rF:
+        topDoc = []
+        for line in rF:
+            t, _, sent, rank, score, run = line.split()
+            if topic == t and int(rank) <= topK:
+                doc = sent.split('_')[0]
+                topDoc.append(doc)
+    return topDoc
 
 def rerank_doc(topic, runF):
-	score_dict = defaultdict(list) 
-	with open(runF) as rF:
-		topDoc = []
-		for line in rF:
-			t, _, sent, rank, score, run = line.split()
-			if topic == t:
-				doc = sent.split('_')[0]
-				score_dict[doc].append(float(score))
-	final_score_dict = defaultdict(float)
-	for doc in score_dict:
-		# final_score_dict[doc] = np.mean(score_dict[doc])
-		# final_score_dict[doc] = sum(score_dict[doc])
-		final_score_dict[doc] = max(score_dict[doc])
-	sorted_doc = sorted(final_score_dict.items(), key=operator.itemgetter(1), reverse=True)
-	rank = 1
-	for doc, score in sorted_doc:
-		print topic, 'Q0', doc, rank, score, 'Sum'
-		rank+=1
+    score_dict = defaultdict(list) 
+    with open(runF) as rF:
+        topDoc = []
+        for line in rF:
+            t, _, sent, rank, score, run = line.split()
+            if topic == t:
+                doc = sent.split('_')[0]
+                score_dict[doc].append(float(score))
+    final_score_dict = defaultdict(float)
+    for doc in score_dict:
+        # final_score_dict[doc] = np.mean(score_dict[doc])
+        # final_score_dict[doc] = sum(score_dict[doc])
+        final_score_dict[doc] = max(score_dict[doc])
+    sorted_doc = sorted(final_score_dict.items(), key=operator.itemgetter(1), reverse=True)
+    rank = 1
+    for doc, score in sorted_doc:
+        print topic, 'Q0', doc, rank, score, 'Sum'
+        rank+=1
 
 def load_nist_qrels():
     rel_dict = defaultdict(list) 
@@ -47,24 +47,30 @@ def load_nist_qrels():
                 nonrel_dict[topic].append(doc)
     return rel_dict, nonrel_dict, all_dict
 
-def main():
-    rel_dict, nonrel_dict, all_dict = load_nist_qrels()
+def calc_recall(rel_dict, topK=1500):
     recall_list = []
     doc_cnt_list = []
     for topic in rel_dict:
-    	rerank_doc(topic,'run.robust04.bm25.topics.robust04.301-450.601-700.txt')
-    # 	topDoc = load_topK_doc_run(1000, topic,
-    # 		'run.robust04.bm25.topics.robust04.301-450.601-700.txt')
-    # 	ret = 0
-    # 	rel = len(rel_dict[topic])
-    # 	for doc in set(topDoc):
-    # 		if doc in rel_dict[topic]:
-    # 			ret+=1
-    # 	# print topic, ret*1.0/rel
-    # 	recall_list.append(ret*1.0/rel)
-    # 	doc_cnt_list.append(len(set(topDoc)))
-    # print np.mean(recall_list)
-    # print np.mean(doc_cnt_list)
+        topDoc = load_topK_doc_run(topK, topic,
+            'run.robust04.bm25.topics.robust04.301-450.601-700.txt')
+        ret = 0
+        rel = len(rel_dict[topic])
+        for doc in set(topDoc):
+            if doc in rel_dict[topic]:
+                ret+=1
+        # print topic, ret*1.0/rel
+        recall_list.append(ret*1.0/rel)
+        doc_cnt_list.append(len(set(topDoc)))
+    print np.mean(recall_list)
+    print np.mean(doc_cnt_list)
+
+
+def main():
+    rel_dict, nonrel_dict, all_dict = load_nist_qrels()
+    calc_recall(rel_dict)
+    return
+    for topic in rel_dict:
+        rerank_doc(topic,'run.robust04.bm25.topics.robust04.301-450.601-700.txt')
 
 if __name__ == "__main__":
     main()
