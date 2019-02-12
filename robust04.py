870447735a442d5b8cbcb45ad35522991cc1c224
Victor0118 <547106693@qq.com>
2019-02-12 18:22:17 -0500

    add robust04

---
+++ robust04.py
@@ -0,0 +1,102 @@
+import string
+printable = set(string.printable)
+printable.remove("\n")
+printable.remove("\t")
+printable.remove("\r")
+import os
+import shlex
+import subprocess
+import sys
+
+from searcher import *
+import searcher
+
+def get_qid2query(ftopic):
+    qid2query = {}
+    f = open(ftopic)
+    query_tag = "title"
+    empty = False
+    for l in f:
+        if empty == True:
+            qid2query[qid] = l.replace("\n", "").strip()
+            empty = False
+        ind = l.find("Number: ")
+        if ind >= 0:
+            qid = l[ind+8:-1]
+            qid = str(int(qid))
+        ind = l.find("<{}>".format(query_tag))
+        if ind >= 0:
+            query = l[ind+8:-1].strip()
+            if len(query) == 0:
+                empty = True
+            else:
+                qid2query[qid] = query
+    return qid2query
+
+def parse_doc_from_index(doc):
+    return doc
+
+def search_document(searcher, prediction_fn, qid2text, output_fn, qid2reldocids, K=1000):
+    f = open(prediction_fn, "w")
+    out = open(output_fn, "w")
+    method = "bm25"
+    for qid in qid2text:
+        a = qid2text[qid]
+        hits = searcher.search(JString(a), K)
+        for i in range(len(hits)):
+            sim = hits[i].score
+            docno = hits[i].docid
+            label = 1 if qid in qid2reldocids and docno in qid2reldocids[qid] else 0
+            b = parse_doc_from_index(hits[i].content)
+            b = "".join(filter(lambda x: x in printable, b))
+            f.write("{} 0 {} 0 {} {}\n".format(qid, docno, sim, method))
+            out.write("{}\t{}\t{}\t{}\t{}\n".format(label, a, b, qid, docno))
+            out.flush()
+    f.close()
+    out.close()
+
+def get_qid2reldocids(fqrel):
+    f = open(fqrel)
+    qid2reldocids = {}
+    for l in f:
+        qid, _, docid, score = l.replace("\n", "").strip().split()
+        if score != "0":
+            if qid not in qid2reldocids:
+                qid2reldocids[qid] = set()
+            qid2reldocids[qid].add(docid)
+    return qid2reldocids
+
+def cal_score(fn_qrels="../Anserini/src/main/resources/topics-and-qrels/qrels.robust2004.txt", prediction="score.txt"):
+    cmd = "/bin/sh run_eval_new.sh {} {}".format(prediction, fn_qrels)
+    pargs = shlex.split(cmd)
+    p = subprocess.Popen(pargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    pout, perr = p.communicate()
+    print("running {}".format(cmd))
+    if sys.version_info[0] < 3:
+        lines = pout.split('\n')
+    else:
+        lines = pout.split(b'\n')
+    Map = float(lines[0].strip().split()[-1])
+    Mrr = float(lines[1].strip().split()[-1])
+    P20 = float(lines[2].strip().split()[-1])
+    P30 = float(lines[3].strip().split()[-1])
+    NDCG20 = float(lines[4].strip().split()[-1])
+    print(Map)
+    print(Mrr)
+    print(P30)
+    print(P20)
+    print(NDCG20)
+    return Map, Mrr, P30, P20, NDCG20
+
+if __name__ == '__main__':
+    fqrel = "../Anserini/src/main/resources/topics-and-qrels/qrels.robust2004.txt"
+    qid2reldocids = get_qid2reldocids(fqrel)
+    ftopic = "../Anserini/src/main/resources/topics-and-qrels/topics.robust04.301-450.601-700.txt"
+    qid2text = get_qid2query(ftopic)
+    prediction_fn = "predict_robust04_bm25.txt"
+    output_fn = "data/robust04_bm25.txt"
+    index_path="/tuna1/indexes/lucene-index.robust04.pos+docvectors+rawdocs"
+    searcher = build_searcher(index_path=index_path)
+    # searcher = build_searcher()
+    search_document(searcher, prediction_fn, qid2text, output_fn, qid2reldocids)
+    cal_score(prediction=prediction_fn)
