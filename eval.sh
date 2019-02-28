cee8b09da3edc73e057b196e8124a3f40c2bee03
HTAustin <socool.king@gmail.com>
2019-02-27 22:41:08 -0500

    Add interpolation

--- eval.sh
+++ eval.sh
@@ -1,10 +1,10 @@
 #!/bin/csh -f
-for i in `seq 1 10`;
+for i in `seq 0.4 0.01 0.6`;
 do
-	python eval_bert.py $i > "run.robust04.bert.tweet.top$i.txt"
+	python eval_bert.py 2 $i > "run.robust04.bert.tweet.top$i.txt"
 	judgement=qrels.robust2004.txt
 	run="run.robust04.bert.tweet.top$i.txt"
 	./trec_eval.9.0/trec_eval -q -c -M 100 ${judgement} ${run} > ${run}.nist.treceval
 	map=`tail -29 ${run}.nist.treceval | grep ^map`
-	echo $map "top$i"
+	echo $map "lambda$i"
 done
--- eval_bert.py
+++ eval_bert.py
@@ -21,14 +21,16 @@ def load_nist_qrels():
 def load_q_doc_bm25(bm25F):
     doc_dict = {}
     q_dict = {}
+    score_dict = {}
     with open(bm25F) as bF:
         for line in bF:
             _, score, q, d, qid, did, qno, dno = line.strip().split('\t')
             doc_dict[dno] = did
             q_dict[qno] = qid
-    return doc_dict, q_dict
+            score_dict[(qno,did.split('_')[0])] = float(score)
+    return doc_dict, q_dict, score_dict
 
-def load_q_doc_bert(bertF, doc_dict, q_dict, topK):
+def load_q_doc_bert(bertF, doc_dict, q_dict, bm25_dict, topK, w):
     score_dict = defaultdict(dict)
     with open(bertF) as bF:
         for line in bF:
@@ -46,18 +48,31 @@ def load_q_doc_bert(bertF, doc_dict, q_dict, topK):
             scores = score_dict[q][d]
             scores.sort(reverse=True)
             # assert(len(scores) > 5) 
-            doc_dict[d] = sum(scores[:topK])
+            sum_score = 0
+            score_list = []
+            weight = 1.0
+            rank = 1.0
+            for s in scores[:topK]:
+            	# sum_score += s
+            	score_list.append(s)
+            # if len(scores) > 1:
+                sum_score += s / rank
+                rank += 1
+            doc_dict[d] = w * bm25_dict[(q,d)] + (1.0-w) * sum_score
+
         doc_dict = sorted(doc_dict.items(), key=operator.itemgetter(1), reverse=True)
         rank = 1
         for doc, score in doc_dict:
-            print q_dict[q], 'Q0', doc, rank, score, 'TopK'
+            print q_dict[q], 'Q0', doc, rank, score, w
             rank+=1
 
 def main():
     topK = int(sys.argv[1])
+    w = float(sys.argv[2])
     rel_dict, nonrel_dict, all_dict = load_nist_qrels()
-    doc_dict, q_dict = load_q_doc_bm25('robust04_bm25_test.csv')
-    load_q_doc_bert('prediction.trec_tweet_title', doc_dict, q_dict, topK)
+    doc_dict, q_dict, score_dict = load_q_doc_bm25('robust04_bm25_test.csv')
+    load_q_doc_bert('prediction.trec_tweet_title', doc_dict, q_dict,
+    	score_dict, 2, w)
 
 if __name__ == "__main__":
     main()
