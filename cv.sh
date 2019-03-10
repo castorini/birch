709878873c295924a1f8d27dc98987f2fd6a7fcf
HTAustin <socool.king@gmail.com>
2019-03-10 00:09:41 -0500

    cv runs

---
+++ cv.sh
@@ -0,0 +1,5 @@
+python eval_bert_test.py 2 0.83 0.2 0 > run.cv.0
+python eval_bert_test.py 2 0.83 0.2 1 > run.cv.1
+python eval_bert_test.py 2 0.83 0.4 2 > run.cv.2
+python eval_bert_test.py 2 0.83 0.2 3 > run.cv.3
+python eval_bert_test.py 2 0.83 0.2 4 > run.cv.4
