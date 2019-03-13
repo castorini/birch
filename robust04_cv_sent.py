c1416864dfcc02a8a06cd4a5024439b2b2ea6956
HTAustin <socool.king@gmail.com>
2019-03-12 23:30:40 -0400

    Small fix

--- robust04_cv_sent.py
+++ robust04_cv_sent.py
@@ -12,8 +12,8 @@ sys.setdefaultencoding('utf-8')
 import re
 import json
 
-from searcher import *
-import searcher
+# from searcher import *
+# import searcher
 
 import nltk
 nltk.download('punkt')
@@ -40,9 +40,8 @@ def clean_html(html):
     cleaned = re.sub(r"(?s)<.*?>", " ", cleaned)
     # Finally, we deal with whitespace
     cleaned = re.sub(r"&nbsp;", " ", cleaned)
+    cleaned = re.sub(r"  ", " ", cleaned)
     cleaned = re.sub(r"\t", " ", cleaned)
-    cleaned = re.sub(r"\n", " ", cleaned)
-    cleaned = re.sub(r"\s+", " ", cleaned)
     return cleaned.strip()
 
 def chunk_sent(sent, max_len):
@@ -80,31 +79,30 @@ def get_qid2query(ftopic):
 def parse_doc_from_index(doc):
     return doc
 
-def search_document(topics, searcher, prediction_fn, qid2text, qid2desc,
-   output_fn, qid2reldocids, qidx, didx, K=1000):
+def search_document(score_file, prediction_fn,
+   output_fn, K=1000):
     # f = open(prediction_fn, "w")
     out = open(output_fn, "w")
     method = "rm3"
-    for qid in topics:
-        a = qid2text[qid]
-        desc = qid2desc[qid]
-        hits = searcher.search(JString(a), K)
-        for i in range(len(hits)):
-            sim = hits[i].score
-            docno = hits[i].docid
-            label = 1 if qid in qid2reldocids and docno in qid2reldocids[qid] else 0
-            b = parse_doc_from_index(hits[i].content)
+    didx = 0
+    with open(score_file) as sF:
+        for line in sF:
+            label, a, b, qid, docno, sim = line.strip().split('\t')
+            # a = qid2text[qid]
+            sim = float(sim)
+            # label = 1 if qid in qid2reldocids and docno in qid2reldocids[qid] else 0
+            # b = parse_doc_from_index(hits[i].content)
             # b = "".join(filter(lambda x: x in printable, b))
             clean_b = clean_html(b)
             sent_id = 0
-            # f.write("{} 0 {} 0 {} {}\n".format(qid, docno, sim, method))
             for sentence in tokenizer.tokenize(clean_b):
                 if len(sentence.strip().split()) > 512:
                     seq_list = chunk_sent(sentence, 512)
                     for seq in seq_list:
                         sentno = docno + '_'+ str(sent_id)
                         # f.write("{} 0 {} 0 {} {}\n".format(qid, sentno, sim, method))
-                        out.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(label, sim, a, seq, qid, sentno, qidx, didx))
+                        out.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format
+                            (label, sim, a, seq, qid, sentno, qid, didx))
                         out.flush()
                         sent_id += 1
                         didx += 1
@@ -112,14 +110,12 @@ def search_document(topics, searcher, prediction_fn, qid2text, qid2desc,
                     sentno = docno + '_'+ str(sent_id)
                     # f.write("{} 0 {} 0 {} {}\n".format(qid, sentno, sim, method))
                     out.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(label,\
-                        sim, a, sentence, qid, sentno, qidx, didx))
+                        sim, a, sentence, qid, sentno, qid, didx))
                     out.flush()
                     sent_id += 1
                     didx += 1
-        qidx += 1
     # f.close()
     out.close()
-    return qidx, didx
 
 def get_qid2reldocids(fqrel):
     f = open(fqrel)
@@ -132,62 +128,17 @@ def get_qid2reldocids(fqrel):
             qid2reldocids[qid].add(docid)
     return qid2reldocids
 
-def get_qid2desc(fdesc):
-    f = open(fdesc)
-    qid2desc = {}
-    for l in f:
-        qid, desc = l.strip().split('\t')
-        qid2desc[qid]= desc
-    return qid2desc
 
 
-def cal_score(fn_qrels="../Anserini/src/main/resources/topics-and-qrels/qrels.robust2004.txt", prediction="score.txt"):
-    cmd = "/bin/sh run_eval_new.sh {} {}".format(prediction, fn_qrels)
-    pargs = shlex.split(cmd)
-    p = subprocess.Popen(pargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    pout, perr = p.communicate()
-    print("running {}".format(cmd))
-    if sys.version_info[0] < 3:
-        lines = pout.split('\n')
-    else:
-        lines = pout.split(b'\n')
-    Map = float(lines[0].strip().split()[-1])
-    Mrr = float(lines[1].strip().split()[-1])
-    P20 = float(lines[2].strip().split()[-1])
-    P30 = float(lines[3].strip().split()[-1])
-    NDCG20 = float(lines[4].strip().split()[-1])
-    print(Map)
-    print(Mrr)
-    print(P30)
-    print(P20)
-    print(NDCG20)
-    return Map, Mrr, P30, P20, NDCG20
 
 if __name__ == '__main__':
-    fqrel = "../src/main/resources/topics-and-qrels/qrels.robust2004.txt"
-    with open('robust04-paper2-folds.json') as f:
-        folds = json.load(f)
-    with open('robust04-paper2-folds-map-params.json') as f:
-        params = json.load(f)
+    # fqrel = "../src/main/resources/topics-and-qrels/qrels.robust2004.txt"
 
-    qid2reldocids = get_qid2reldocids(fqrel)
-    ftopic = "../src/main/resources/topics-and-qrels/topics.robust04.301-450.601-700.txt"
-    qid2text = get_qid2query(ftopic)
-    qid2desc = get_qid2desc('topics.desc')
+    # qid2reldocids = get_qid2reldocids(fqrel)
+    # ftopic = "../src/main/resources/topics-and-qrels/topics.robust04.301-450.601-700.txt"
+    # qid2text = get_qid2query(ftopic)
     prediction_fn = "predict_robust04_rm3_cv.txt"
-    output_fn = "robust04_bm25_rm3_cv.txt"
-    index_path="/tuna1/indexes/lucene-index.robust04.pos+docvectors+rawdocs"
-    folder_idx = 1
-    qidx, didx = 1, 1
-    for topics, p in zip(folds, params):
-        a, b, c, d, e = map(float, p.strip().split())
-        searcher = build_searcher(k1=a, b=b,
-            fbTerms=c, fbDocs=d, originalQueryWeight=e,
-            index_path=index_path, rm3=True)
-        # searcher = build_searcher()
-        qidx, didx = search_document(topics, searcher, 
-            prediction_fn+str(folder_idx), 
-            qid2text, qid2desc, output_fn+str(folder_idx), qid2reldocids,
-            qidx, didx, 1000)
-        folder_idx += 1
-        # cal_score(prediction=prediction_fn)
+    output_fn = "robust04_bm25_rm3_cv_sent.txt"
+
+    search_document('robust04_bm25_rm3_cv.txt', prediction_fn, output_fn, 1000)
+
