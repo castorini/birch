b52a2db8c09e0675357b1c65a08861cfdf51ff1f
Zeynep Akkalyoncu Yilmaz <zakkalyoncu@uwaterloo.ca>
2019-04-30 16:12:11 -0400

    Update predict script

--- predict.sh
+++ predict.sh
@@ -2,7 +2,7 @@
 
 experiment='model-name'
 python main.py --mode test --batch_size 8 --data_path data --data_name robust04 \
-    --pytorch_dump_path saved.finetune_1 --load_trained \
+    --pytorch_dump_path saved.${experiment} --load_trained \
     --trec_eval_path ../Anserini/eval/trec_eval.9.0.4/trec_eval \
     --eval_steps 1000 --output_path out.${experiment} \
     --predict_path predict.${experiment}
