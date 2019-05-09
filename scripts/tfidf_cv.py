5ecf47c6a6bbc68972c7d3cd03d2eeddc8505691
Zeynep Akkalyoncu Yilmaz <zakkalyoncu@uwaterloo.ca>
2019-05-08 23:47:45 -0400

    Exclude docs

--- scripts/tfidf_cv.py
+++ scripts/tfidf_cv.py
@@ -26,40 +26,42 @@ assert(len(dev_topics) == 25)
 assert(len(test_topics) == 125)
 assert(len(all_topics) == 250)
 
+# Create test qrels
+sent2doc = {}
+with open('data/robust04/robust04_test.csv', 'r') as robust04_file:
+    lines = robust04_file.readlines()
+    for line in lines:
+        _, _, _, _, _, docid, _, sentid = line.rstrip().split('\t')
+        docid = docid.split('_')[0]
+        sent2doc[sentid] = docid
+
+labels = {}
+with open('data/qrels.robust2004.txt', 'r') as original_qrels:
+    qrels = original_qrels.readlines()
+    for qrel in qrels:
+        qid, _, docid, label = qrel.rstrip().split()
+        if qid in dev_topics or qid in test_topics:
+            if qid not in labels.keys():
+                labels[qid] = {}
+            labels[qid][docid] = label
+
 with open('data/tfidf_sents/tfidf_sents.csv', 'r') as sents_file, \
         open('data/tfidf_sents/tfidf_sents_train.csv', 'w') as train_file, \
         open('data/tfidf_sents/tfidf_sents_dev.csv', 'w') as dev_file, \
-        open('data/tfidf_sents/tfidf_sents_test.csv', 'w') as test_file:
+        open('data/tfidf_sents/tfidf_sents_test.csv', 'w') as test_file, \
+        open('data/qrels.tfidf.txt', 'w') as tfidf_qrels:
     sents = sents_file.readlines()
     for sent in sents:
         sent_tokens = sent.rstrip().split('\t')
         qid = sent_tokens[3].split()[0]
+        sentid = sent_tokens[3].split()[2]
         if qid in train_topics:
             train_file.write(sent)
-        elif qid in dev_topics:
-            dev_file.write(sent)
         else:
-            test_file.write(sent)
-
-# Create test qrels
-doc2sent = {}
-with open('data/robust04/robust04_test.csv', mode='r') as robust04_file:
-    lines = robust04_file.readlines()
-    for line in lines:
-        _, _, _, _, _, docid, qid, sentid = line.rstrip().split('\t')
-        docid = docid.split('_')[0]
-        if qid not in doc2sent.keys():
-            doc2sent[qid] = {}
-        if docid not in doc2sent[qid].keys():
-            doc2sent[qid][docid] = []
-        doc2sent[qid][docid].append(sentid)
-
-with open('data/qrels.tfidf.txt', 'w') as tfidf_qrels, \
-    open('data/qrels.robust2004.txt', 'r') as original_qrels:
-    qrels = original_qrels.readlines()
-    for qrel in qrels:
-        qid, _, docid, label = qrel.rstrip().split()
-        if (qid in test_topics or qid in dev_topics) and docid in doc2sent[qid].keys():
-            sentids = doc2sent[qid][docid]
-            for sentid in sentids:
-                tfidf_qrels.write('{} 0 {} {}\n'.format(qid, sentid, label))
+            docid = sent2doc[sentid]
+            if qid in labels.keys() and docid in labels[qid].keys():
+                tfidf_qrels.write('{} {} {} {}\n'.format(qid, 0, sentid, labels[qid][docid]))
+            if qid in dev_topics:
+                dev_file.write(sent)
+            else:
+                test_file.write(sent)
